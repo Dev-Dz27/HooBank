c2f857a9b3dca79549e122eb79f0d06004c73599
Chekkal Youcef <81917154+Dev-Dz27@users.noreply.github.com>
2023-01-08 17:20:52 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
+
 const Navbar = () => {
 const [toggle, setToggle] = useState(false)
 
